c238d8890b65fbc193c8904bc6df40b3b135386a
ZJYCP <chnzjycp@foxmail.com>
2019-10-08 17:31:35 +0800

    模板信息发送

--- server/app/Http/Controllers/Api/MicroController.php
+++ server/app/Http/Controllers/Api/MicroController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Device;
 use App\Enviroment;
 use Illuminate\Http\Request;
 use Log;
@@ -9,6 +10,43 @@ use Log;
 class MicroController extends Controller
 {
     //
+
+    function __construct()
+    {
+    }
+
+
+    /**
+     * 获取符合条件的用户列表
+     * @param int $device_id
+     * @return mixed
+     */
+    private function getUserOpenid($device_id = 520)
+    {
+        return Device::select('user_id')->where('device_id', $device_id)->get();
+    }
+
+    /**
+     * 发送微信模板消息
+     * @param $touser
+     * @param $template_id
+     * @param $data
+     * @return mixed
+     */
+    private function send_template($touser, $template_id, $data)
+    {
+        $app = app('wechat.official_account');
+
+        $res = $app->template_message->send([
+            'touser' => $touser,
+            'template_id' => $template_id,
+            'url' => 'https://blog.emx6.com',
+            'scene' => 1000,
+            'data' => $data
+        ]);
+
+        return $res;
+    }
 
     /**
      * 温度、湿度、噪声强度数据新增
@@ -43,22 +81,29 @@ class MicroController extends Controller
      */
     public function alarm($type)
     {
-        $app = app('wechat.official_account');
 
-        if($type=='infrared'){
-            $res = $app->template_message->send([
-                'touser' => 'oLCjis0yxRyqcoeMGbC57g-BQiKY',
-                'template_id' => 'ywUbmiyxDXBMD13qsv7yNTqW7kNQK_hXyRfbmog-U-c',
-                'url' => 'https://blog.emx6.com',
-                'scene' => 1000,
-                'data' => [
-                    'temp' => '23.3',
-                    'level' => 'high',
-                ],
-            ]);
-            if($res['errcode']==0){
+        $users = $this->getUserOpenid(520);
+        if ($type == 'infrared') {
+
+            $template_id = 'ywUbmiyxDXBMD13qsv7yNTqW7kNQK_hXyRfbmog-U-c';
+            $data = [
+                'temp' => '23.3',
+                'level' => date('Y-m-d h:i:s', time()),
+            ];
+            $res_flag = 1;
+            foreach ($users as $value) {
+                $res = $this->send_template($value['user_id'], $template_id, $data);
+                if ($res['errcode'] != 0) {
+                    $res_flag = 0;
+                }
+            }
+
+            if ($res_flag){
                 Log::info('send infrared alarm successfully');
-                return $this->success('红外模板消息发送成功');
+
+                return $this->success('红外模板消息发送成功'.'共有'.count($users).'人');
+            }else{
+                return $this->failed('模板发送失败',400,1002);
             }
         }
 
