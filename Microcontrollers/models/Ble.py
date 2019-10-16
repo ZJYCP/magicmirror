76abeecdec6acf54418388fc0280bb7e74a3f491
Glyn <chnzjycp@foxmail.com>
2019-10-16 18:30:02 +0800

    add serial

--- Microcontrollers/models/Ble.py
+++ Microcontrollers/models/Ble.py
@@ -14,19 +14,31 @@ import threading
 
 
 class BLE:
+    ser = serial.Serial("/dev/ttyAMA0", 115200) 
     port = ''
     baud_rate = 115200
 
     def __init__(self):
         # self.ser = serial.Serial(self.port, self.baud_rate)
+        self.ser.flushInput()  
         self.sensors = []
         #self.listen_msg()
 
     # 监听
     def listen_msg(self):
         data = {'code': 1,'data': 1}
-        if True:
-            self.notifySensor(data)
+        while True:
+            count = self.ser.inWaiting()  
+            if count != 0:
+                recv = self.ser.read(count)  
+                #ser.write("aaa".encode('utf-8'))  
+                #ser.write(recv)  
+                aa=''.join(['%02x' % b for b in recv])
+                print(aa)
+                self.ser.flushInput()
+            time.sleep(0.1)
+        #if True:
+            #self.notifySensor(data)
 
     # 通知
     def notifySensor(self, data):
@@ -38,7 +50,6 @@ class BLE:
         self.sensors.append(sensor)
 
 
-if __name__ == 'main':
-    pass
-    # blue = BLE()
-    # blue.listen_msg()
+#if __name__ == 'main':
+#blue = BLE()
+#blue.listen_msg()
